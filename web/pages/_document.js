100c8cb77eabc7cc517eca8718aac94fa4379ef9
aqilusman45 <aqilusman45@gmail.com>
2024-02-03 14:29:32 +0500

    add favicon

--- web/pages/_document.js
+++ web/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.jpg" />
+        <link rel="icon" href="/favicon.svg" />
       </Head>
       <body>
         <Main />
